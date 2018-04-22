f0d5dbfb90c6e5a09e3c1a0062fbbce4b969d6e2
Akindele Akinyemi <akbumper@gmail.com>
2018-04-22 17:44:48 +0100

    finished

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  taxid: { type: String, unique: true },
+  firstname: { type: String }
+
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
